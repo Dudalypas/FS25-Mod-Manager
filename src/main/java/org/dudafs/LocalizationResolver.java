a2c4821e7d9c9746196d1c0fcbf238063af4afdb
Dudalypas <viljamassesko4@gmail.com>
2026-07-16 15:05:52 +0300

    Add more localizations to resolver

--- src/main/java/org/dudafs/LocalizationResolver.java
+++ src/main/java/org/dudafs/LocalizationResolver.java
@@ -6,7 +6,10 @@ public interface LocalizationResolver {
     static final Map<String, String> FALLBACK_TRANSLATIONS = Map.of(
             "$l10n_info_transmission_manual", "Manual",
             "$l10n_info_transmission_automatic", "Automatic",
-            "$l10n_info_transmission_cvt", "CVT"
+            "$l10n_info_transmission_cvt", "Variable (CVT)",
+            "$l10n_info_transmission_manualWithPowerShiftGroups", "Manual + Powershift",
+            "$l10n_info_transmission_powerShift", "PowerShift",
+            "$l10n_info_transmission_dynaVT", "DynaVT"
     );
 
     static String resolve(String key){
